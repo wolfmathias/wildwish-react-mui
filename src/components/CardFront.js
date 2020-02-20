b814e82ef5a38101a24c22632b8b3d5877db8074
bigcatplichta <bigcatplichta@gmail.com>
2020-02-20 16:33:48 -0700

    added collapsable section

--- src/components/CardFront.js
+++ src/components/CardFront.js
@@ -6,24 +6,53 @@ import Link from '@material-ui/core/Link';
 
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Button from '@material-ui/core/Button';
+
 
 import boleroImg from './bolero.JPG'
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
     maxWidth: 400,
   },
-});
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  }
+}));
 
 export default function CardFront() {
   const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   return (
     <Card className={classes.root}>
@@ -40,18 +69,43 @@ export default function CardFront() {
             Bolero
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-            Bolero is an African Lion residing at a wildlife sanctuary in California.
+            California, USA
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
+        
+       
       </CardActions>
+      <CardActions disableSpacing>
+        <Button size="small" color="primary" onClick={handleExpandClick}>
+          ABOUT BOLERO
+        </Button>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          disableRipple
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="about animal"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>
+            Info about each animal goes in this expanded card.
+          </Typography>
+          <Typography paragraph>
+            Card content includes animal bio and information about the enrichment for the Wish.
+          </Typography>
+          <Typography paragraph>
+            The bio section will have a character limit so content in cards do not need to be truncated.
+          </Typography>
+        </CardContent>
+      </Collapse>
     </Card>
   );
 }
