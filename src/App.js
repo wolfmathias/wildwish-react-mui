0d8098cce495313375bfbdc30a5a083de177ec4b
bigcatplichta <bigcatplichta@gmail.com>
2020-02-20 00:04:45 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import ProTip from './ProTip';
+
+import CardFront from './components/CardFront'
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+// App renders navigation bar
+// If user is logged out landing page will be displayed
+export default function App() {
+  return (
+    <Container maxWidth="sm">
+      <CardFront />
+      {/* <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App v4-beta example
+        </Typography>
+        <ProTip />
+        <Copyright />
+      </Box> */}
+    </Container>
+  );
+}
