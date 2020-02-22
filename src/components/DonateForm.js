2122fde4b112b6449e6af207fdf38cdbece7b873
bigcatplichta <bigcatplichta@gmail.com>
2020-02-22 12:05:17 -0700

    added fields

--- src/components/DonateForm.js
+++ src/components/DonateForm.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper'
 
+// TODO: style so form inputs look good on form
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
@@ -11,7 +12,7 @@ const useStyles = makeStyles(theme => ({
   textField: {
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
-    width: 200,
+    width: 173,
   },
 }));
 
@@ -20,7 +21,9 @@ export default function DonateForm() {
 
     return (
         <Paper>
+        
         <form className={classes.root}>
+            
             <TextField
                 id="email"
                 label="Email"
@@ -35,7 +38,30 @@ export default function DonateForm() {
                 variant="outlined"
                 required
             />
-            
+            <TextField
+                id="firstName"
+                label="First Name"
+                style={{ margin: 8 }}
+                
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                variant="outlined"
+                className={classes.textField}
+                required
+            />
+            <TextField
+                id="lastName"
+                label="Last Name"
+                style={{ margin: 8 }}
+                
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                variant="outlined"
+                className={classes.textField}
+                required
+            />
         </form>
         </Paper>
     )
