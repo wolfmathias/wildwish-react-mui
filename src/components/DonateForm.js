882b186c0cc76a5c506ba589f9365527be35c9f4
bigcatplichta <bigcatplichta@gmail.com>
2020-02-22 12:51:14 -0700

    added buttons

--- src/components/DonateForm.js
+++ src/components/DonateForm.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import Paper from '@material-ui/core/Paper'
+import Paper from '@material-ui/core/Paper';
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
 
 // TODO: style so form inputs look good on form
 const useStyles = makeStyles(theme => ({
@@ -21,48 +24,53 @@ export default function DonateForm() {
 
     return (
         <Paper>
-        
-        <form className={classes.root}>
-            
-            <TextField
-                id="email"
-                label="Email"
-                style={{ margin: 8 }}
-                placeholder=""
-                helperText=""
-                fullWidth
-                margin="normal"
-                InputLabelProps={{
-                shrink: true,
-                }}
-                variant="outlined"
-                required
-            />
-            <TextField
-                id="firstName"
-                label="First Name"
-                style={{ margin: 8 }}
-                
-                InputLabelProps={{
+            <Typography variant="body1">Donate to Bolero</Typography>
+            <form className={classes.root}>
+                <ButtonGroup color="primary" aria-label="outlined primary button group">
+                    <Button>One</Button>
+                    <Button>Two</Button>
+                    <Button>Three</Button>
+                </ButtonGroup>
+                <TextField
+                    id="email"
+                    label="Email"
+                    style={{ margin: 8 }}
+                    placeholder=""
+                    helperText=""
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
                     shrink: true,
-                }}
-                variant="outlined"
-                className={classes.textField}
-                required
-            />
-            <TextField
-                id="lastName"
-                label="Last Name"
-                style={{ margin: 8 }}
-                
-                InputLabelProps={{
-                    shrink: true,
-                }}
-                variant="outlined"
-                className={classes.textField}
-                required
-            />
-        </form>
+                    }}
+                    variant="outlined"
+                    required
+                />
+                <TextField
+                    id="firstName"
+                    label="First Name"
+                    style={{ margin: 8 }}
+                    
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    variant="outlined"
+                    className={classes.textField}
+                    required
+                />
+                <TextField
+                    id="lastName"
+                    label="Last Name"
+                    style={{ margin: 8 }}
+                    
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    variant="outlined"
+                    className={classes.textField}
+                    required
+                />
+                <Button variant="contained" size="large" color="primary">Donate</Button>
+            </form>
         </Paper>
     )
 
