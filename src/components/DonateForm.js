2c9d59a707d00d3aba727fba9f6d8066e69fb359
bigcatplichta <bigcatplichta@gmail.com>
2020-02-22 10:29:56 -0700

    add DonateForm

---
+++ src/components/DonateForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+}));
