0d48983a7cf6490f296777e3ed0530885f739d4d
bigcatplichta <bigcatplichta@gmail.com>
2020-02-22 18:32:15 -0700

    add checkbox

--- src/components/DonateForm.js
+++ src/components/DonateForm.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
-import ButtonGroup from '@material-ui/core/ButtonGroup'
-import Button from '@material-ui/core/Button'
-import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio'
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl'
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox'
 
 // TODO: style so form inputs look good on form
 const useStyles = makeStyles(theme => ({
@@ -21,16 +26,25 @@ const useStyles = makeStyles(theme => ({
 
 export default function DonateForm() {
     const classes = useStyles();
+    const [state, setState] = React.useState({
+        checked: true
+    });
+
+    const handleChange = () => {
+    setState({checked: !state.checked});
+    };
 
     return (
         <Paper>
             <Typography variant="body1">Donate to Bolero</Typography>
             <form className={classes.root}>
-                <ButtonGroup color="primary" aria-label="outlined primary button group">
-                    <Button>One</Button>
-                    <Button>Two</Button>
-                    <Button>Three</Button>
-                </ButtonGroup>
+                <RadioGroup color="primary" aria-label="outlined primary button group">
+                    <Grid>
+                        <FormControl value="10" control={<Radio />} label="$10" />
+                        <FormControl value="15" control={<Radio />} label="$15" />
+                        <FormControl value="20" control={<Radio />} label="$20" />
+                    </Grid>
+                </RadioGroup>
                 <TextField
                     id="email"
                     label="Email"
@@ -68,6 +82,17 @@ export default function DonateForm() {
                     variant="outlined"
                     className={classes.textField}
                     required
+                />
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            checked={state.checked}
+                            onChange={handleChange}
+                            value="true"
+                            color="primary"
+                        />
+                    }
+                    label="Create account after donation"
                 />
                 <Button variant="contained" size="large" color="primary">Donate</Button>
             </form>
