16c3ebdf8120b0541c23c77152775414085cab5f
bigcatplichta <bigcatplichta@gmail.com>
2020-02-25 15:57:37 -0700

    moved Animals data

---
+++ src/data/Animals.js
@@ -0,0 +1,35 @@
+const animals = [
+    // http://localhost:3000/animals
+    {
+      id: 29,
+      name: "Tsathoggua",
+      species: "Mouse",
+      bio: null,
+      keeper_id: 5,
+      created_at: "2020-01-30T17:54:03.580Z",
+      updated_at: "2020-01-30T17:54:03.580Z",
+      location: "Cheyenne Mountain Zoo - Colorado, USA"
+    },
+    {
+      id: 132,
+      name: "Viserion",
+      species: "Seal",
+      bio: null,
+      keeper_id: 28,
+      created_at: "2020-01-30T17:54:03.887Z",
+      updated_at: "2020-01-30T17:54:03.887Z",
+      location: "Cat Tales Zoo - Washington, USA"
+    },
+    {
+      id: 160,
+      name: "Hank Hippopopalous",
+      species: "Porcupine",
+      bio: null,
+      keeper_id: 31,
+      created_at: "2020-01-30T17:54:03.972Z",
+      updated_at: "2020-01-30T17:54:03.972Z",
+      location: "Point Defiance Zoo - Washington, USA"
+    }
+  ]
+
+  export default animals
