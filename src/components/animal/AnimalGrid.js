0612b7261b6c9b41ce5aba17bb7a230a4f6b687d
bigcatplichta <bigcatplichta@gmail.com>
2020-02-25 14:30:28 -0700

    added styles

--- src/components/animal/AnimalGrid.js
+++ src/components/animal/AnimalGrid.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import { GridList, GridListTile, ListSubheader } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+import Animal from './Animal';
 
 
+const useStyles = makeStyles(theme => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      overflow: 'hidden',
+      backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+      width: 500,
+      height: 450,
+    },
+    icon: {
+      color: 'rgba(255, 255, 255, 0.54)',
+    },
+  }));
 
 export default function AnimalGrid(props) {
     const classes = useStyles();
     
     const animalCards = props.animals.map(animal => {
-        // return <Animal animal={animal} key={animal.id} />
+        return <Animal animal={animal} key={animal.id} />
     })
 
 
@@ -17,7 +35,9 @@ export default function AnimalGrid(props) {
                 <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
                     <ListSubheader component="div">Animals</ListSubheader>
                 </GridListTile>
+                {animalCards}
             </GridList>
+            
         </div>
     )
 }
