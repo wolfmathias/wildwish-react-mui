1af890e85fb1aac56eaa4807529bff76da289225
bigcatplichta <bigcatplichta@gmail.com>
2020-02-25 14:12:04 -0700

    updated components to render

--- src/components/animal/AnimalGrid.js
+++ src/components/animal/AnimalGrid.js
@@ -1,13 +1,23 @@
 import React from 'react'
+import { GridList, GridListTile, ListSubheader } from '@material-ui/core'
+
+
 
 export default function AnimalGrid(props) {
+    const classes = useStyles();
+    
     const animalCards = props.animals.map(animal => {
-        return <Animal animal={animal} key={animal.id} />
+        // return <Animal animal={animal} key={animal.id} />
     })
 
+
     return (
-        <Grid container>
-            {animalCards}
-        </Grid>
+        <div className={classes.root}>
+            <GridList cellHeight={180} className={classes.gridList}>
+                <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+                    <ListSubheader component="div">Animals</ListSubheader>
+                </GridListTile>
+            </GridList>
+        </div>
     )
 }
