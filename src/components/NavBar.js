d8affd46ea75ec0d61f43a4cd3fc275bce66d205
bigcatplichta <bigcatplichta@gmail.com>
2020-02-26 20:01:26 -0700

    removed routerlink

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,6 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { NavLink } from 'react-router-dom';
-import RouterLink from '@material-ui/core'
 
 const useStyles = makeStyles(theme => ({
   root: {
