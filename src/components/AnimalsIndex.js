2c5f4c5f30e0c0608b23bca7e6801756303a892d
bigcatplichta <bigcatplichta@gmail.com>
2020-02-28 18:22:16 -0700

    played with routes

--- src/components/AnimalsIndex.js
+++ src/components/AnimalsIndex.js
@@ -2,13 +2,16 @@ import React from 'react';
 import AnimalGrid from './animal/AnimalGrid';
 import ShowAnimal from './animal/ShowAnimal';
 import {Route} from 'react-router-dom';
+import NewAnimal from './animal/NewAnimal';
 
 export default function AnimalsIndex({match, animals}) {
    
     return (
         <>
         <Route exact path="/animals" render={routerProps => <AnimalGrid {...routerProps} animals={animals}/>} />
-        <Route path={`${match.url}/:animalId`} render={routerProps => <ShowAnimal {...routerProps} animals={animals} /> }/>
+        <Route exact path={`${match.url}/new`} render={routerProps => <NewAnimal {...routerProps} animals={animals} /> }/>
+        {/* <Route path={`${match.url}/:animalId`} render={routerProps => <ShowAnimal {...routerProps} animals={animals} /> }/> */}
+        
         </>
 
         // Have options for views alternate than grid for main index, use list for larger screens
