a969dc83b23e598e203a82d83209286932cb248d
bigcatplichta <bigcatplichta@gmail.com>
2020-02-28 18:21:29 -0700

    created input mockup

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default function Login() {
-    return (
-        <h1>This is the login page.</h1>
-    )
+class Login extends Component {
+   
+    handleInputChange = (event) => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
+    
+    handleOnSubmit = (event) => {
+        event.preventDefault();
+        this.props.dispatch({type: 'ADD_USER', user: this.state})
+    }
+    
+    render() {
+        return(
+            <form onSubmit={this.handleOnSubmit}>
+            <h1>This is the login page.</h1>
+            <p>
+                <input
+                type="text"
+                id="username"
+                onChange={this.handleInputChange}
+                placeholder="username"
+                />
+            </p>
+            <p>
+                <input
+                type="text"
+                id="hometown"
+                onChange={this.handleInputChange}
+                placeholder="hometown"
+                />
+            </p>
+            <input type="submit" />
+            </form>
+        )
+    }
 }
+
+export default connect()(Login);
