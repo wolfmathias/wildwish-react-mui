60765b03a4287c40285485e36fee5e8e365b0381
bigcatplichta <bigcatplichta@gmail.com>
2020-02-29 01:09:39 -0700

    Cleaned up comments and old examples

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import wishes from './data/Wishes';
 // For router and redux
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { addAnimal } from './actions/animals';
 
 
 
@@ -66,7 +67,64 @@ class App extends React.Component {
     donations: donations
   }
   
-  // For Redux store:
+
+  render() {
+   
+    return (
+      <>
+      {/* <div className="App">
+      <button onClick={this.handleOnClick}>BUTTON FOR PLAYING WITH REDUX STORE</button>
+      <p>{this.props.animalList.length}</p>
+      </div> */}
+      <Router>
+        <NavBar/>
+        <Container>
+            <Route exact path="/" component={Home}/>
+            {/* <Route path="/about" component={About}/> */}
+            <Route path="/animals" render={routerProps => <AnimalsIndex {...routerProps} animals={this.state.animals}/>} />
+            <Route path="/newanimal" render={routerProps => <NewAnimal {...routerProps} animals={this.state.animals}/>} />
+            {/* <Route path="/donations" render={routerProps => <DonationsIndex {...routerProps} donations={this.state.donations}/>} /> */}
+            <Route path="/login" component={Login}/>
+        </Container>
+      </Router>
+      <Copyright/>
+      </>
+    )
+  }
+}
+
+// Take the state from the store and map them to props
+// export default connect(state => ({ animals: state.animals }), { addAnimal: addAnimal })(App);
+
+// Above line is composed of the following:
+const mapStateToProps = state => {
+  return {
+    animals: state.animals
+  };
+};
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addAnimal: () => dispatch({ type: 'ADD_ANIMAL' })
+//   };
+// };
+//
+// OR:
+// Below, addAnimal() can accept a payload of an animal as `addAnimal(animal)`, where `animal` is an object {name: "", species: "", etc}
+const mapDispatchToProps = dispatch => {
+  return {
+    addItem: () => {
+      dispatch(addAnimal())
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
+
+ // For Redux store:
   // createStore(reducer) {
   //   let state;
   
@@ -94,49 +152,6 @@ class App extends React.Component {
   // }
 
   // For playing with redux
-  handleOnClick = event => {
-    this.props.increaseCount();
-  };
-
-  render() {
-   
-    return (
-      <>
-      <div className="App">
-      <button onClick={this.handleOnClick}>BUTTON FOR PLAYING WITH REDUX STORE</button>
-      <p>{this.props.animalList.length}</p>
-      </div>
-      <Router>
-        <NavBar/>
-        <Container>
-            <Route exact path="/" component={Home}/>
-            {/* <Route path="/about" component={About}/> */}
-            <Route path="/animals" render={routerProps => <AnimalsIndex {...routerProps} animals={this.state.animals}/>} />
-            <Route path="/newanimal" render={routerProps => <NewAnimal {...routerProps} animals={this.state.animals}/>} />
-            {/* <Route path="/donations" render={routerProps => <DonationsIndex {...routerProps} donations={this.state.donations}/>} /> */}
-            <Route path="/login" component={Login}/>
-        </Container>
-      </Router>
-      <Copyright/>
-      </>
-    )
-  }
-}
-
-// Take the state from the store and map them to props
-const mapStateToProps = state => {
-  return {
-    animalList: state.animalList
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    increaseCount: () => dispatch({ type: 'ADD_ANIMAL' })
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+  // handleOnClick = event => {
+  //   this.props.increaseCount();
+  // };
