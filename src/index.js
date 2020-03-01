71d8832b3fcb92ad0ddd4e7de10182017bca4b41
bigcatplichta <bigcatplichta@gmail.com>
2020-03-01 13:12:26 -0700

    remove commented line

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ import rootReducer from './reducers/rootReducer';
 
 const store = createStore(
   rootReducer,
-  // manageUsers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
