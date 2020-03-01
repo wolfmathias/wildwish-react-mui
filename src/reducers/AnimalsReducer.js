240295afb3583dd27e59601751e38ffd141e8374
bigcatplichta <bigcatplichta@gmail.com>
2020-03-01 13:43:29 -0700

    added case for ADD_WISH action

--- src/reducers/AnimalsReducer.js
+++ src/reducers/AnimalsReducer.js
@@ -8,16 +8,24 @@ export default function AnimalsReducer(
     switch(action.type) {
         case 'ADD_ANIMAL':
             console.log("adding", action.animal)
-            // Below return written before adding rootReducer
-            // return {
-            //     ...state,
-            //     animals: [...state.animals, action.animal]
-            // };
             return [...state, action.animal];
         // Test below case
         case "REMOVE_ANIMAL":
             idx = state.findIndex(animal => animal.id  === action.id)
             return [...state.slice(0, idx), ...state.slice(idx + 1)];
+
+        // Both AnimalReducer and WishesReducer respond to ADD WISH action
+        // TODO: update prop names to match actual data
+        // Do I even want to handle it this way?
+        case "ADD_WISH":
+            let existingAnimal = state.filter(
+                animal => animal.animalName === action.wish.animalName
+            );
+            if (existingAnimal.length > 0) {
+                return state;
+            } else {
+                return [...state, { animalName: action.wish.animalName, id: uuid() }];
+            }
 
         default:
             return state;
