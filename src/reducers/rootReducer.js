3db7495a713c2c9d931b013c7fee7841ba015455
bigcatplichta <bigcatplichta@gmail.com>
2020-03-01 12:45:15 -0700

    added reducer

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import { AnimalsReducer } from './AnimalsReducer';
+import { WishesReducer } from './WishesReducer';
+ 
+const rootReducer = combineReducers({
+  animals: AnimalsReducer,
+  wishes: WishesReducer
+});
+ 
+export default rootReducer;
