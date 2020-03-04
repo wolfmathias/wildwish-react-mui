68c317e37fbf39de7a3d97704f17ab2c3b853a10
bigcatplichta <bigcatplichta@gmail.com>
2020-03-03 20:00:09 -0700

    add deleteAnimal action

--- src/actions/animals.js
+++ src/actions/animals.js
@@ -1,6 +1,7 @@
 // export const addAnimal = (animal) => {
 //     return { type: 'ADD_ANIMALS', payload: animal };
 // }; 
+import uuid from 'uuid'
 
 export const addAnimal = (animal) => {
     return (dispatch) => {
@@ -13,6 +14,7 @@ export const addAnimal = (animal) => {
             },
             body: JSON.stringify({
                 animal: {
+                    // id: uuid(),
                     name: animal.name,
                     species: animal.species
                 }
@@ -22,6 +24,32 @@ export const addAnimal = (animal) => {
         .then(responseJSON => {
             console.log("Posted to the server!")
             dispatch({ type: 'ADD_ANIMALS', payload: responseJSON })
+        })
+    }
+}
+
+export const deleteAnimal = (animal) => {
+    return (dispatch) => {
+        dispatch({ type: 'LOADING_ANIMALS' })
+        console.log("sending delete request for:", animal)
+        fetch(`http://localhost:3001/animals/${animal.id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                animal: {
+                    id: animal.id,
+                    name: animal.name,
+                    species: animal.species
+                }
+            })
+        })
+        .then(response => response.json())
+        .then(responseJSON => {
+            console.log("Posted to the server!")
+            dispatch({ type: 'DELETE_ANIMAL', payload: responseJSON })
         })
     }
 }
