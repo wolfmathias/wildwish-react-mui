f20e43dac33be994b0e22b3d58c266cc64de5dd1
bigcatplichta <bigcatplichta@gmail.com>
2020-03-03 19:18:31 -0700

    update action to use fetch

--- src/actions/animals.js
+++ src/actions/animals.js
@@ -1,15 +1,39 @@
-export const addAnimal = (animal) => {
-    return { type: 'ADD_ANIMALS', payload: animal };
-}; 
+// export const addAnimal = (animal) => {
+//     return { type: 'ADD_ANIMALS', payload: animal };
+// }; 
 
-export const addAnimalOk = () => ({
-    type: "ADD_ANIMAL_OK"
-});
+export const addAnimal = (animal) => {
+    return (dispatch) => {
+        dispatch({ type: 'LOADING_ANIMALS' })
+        fetch('http://localhost:3001/animals', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                animal: {
+                    name: animal.name,
+                    species: animal.species
+                }
+            })
+        })
+        .then(response => response.json())
+        .then(responseJSON => {
+            console.log("Posted to the server!")
+            dispatch({ type: 'ADD_ANIMALS', payload: responseJSON })
+        })
+    }
+}
+
+// export const addAnimalOk = () => ({
+//     type: "ADD_ANIMAL_OK"
+// });
   
-export const addAnimalFailed = (err) => ({
-    type: "ADD_ANIMAL_FAILED",
-    payload: err
-});
+// export const addAnimalFailed = (err) => ({
+//     type: "ADD_ANIMAL_FAILED",
+//     payload: err
+// });
 
 export const fetchAnimals = () => {
     return (dispatch) => {
@@ -21,22 +45,4 @@ export const fetchAnimals = () => {
         dispatch({ type: 'ADD_ANIMALS', payload: responseJSON })
       })
     }
-}
-
-export const postAnimal = (animal) => {
-    fetch('http://localhost:3000/animals', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        body: JSON.stringify({
-            animal: {
-                name: animal.name,
-                species: animal.species
-            }
-        })
-    })
-    // Do something with the response
-    // .then(do_something)
 }
