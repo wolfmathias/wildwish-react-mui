942bc8e7ed81ad774b3a14588dc3836bcb6dd99c
bigcatplichta <bigcatplichta@gmail.com>
2020-03-04 13:21:30 -0700

    remove unused imports

--- src/index.js
+++ src/index.js
@@ -2,13 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { Provider } from 'react-redux';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/core/styles';
-import App from './App';
-import theme from './theme';
 import rootReducer from './reducers/rootReducer';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Root from './Root';
 
 const store = createStore(
