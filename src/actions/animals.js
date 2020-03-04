723ed93ad48039fece6ece2cea6908abfbff9461
bigcatplichta <bigcatplichta@gmail.com>
2020-03-03 17:38:59 -0700

    add test actions

--- src/actions/animals.js
+++ src/actions/animals.js
@@ -1,6 +1,15 @@
-export const addAnimal = (payload) => {
-    return { type: 'ADD_ANIMALS', payload };
+export const addAnimal = (animal) => {
+    return { type: 'ADD_ANIMALS', payload: animal };
 }; 
+
+export const addAnimalOk = () => ({
+    type: "ADD_ANIMAL_OK"
+});
+  
+export const addAnimalFailed = (err) => ({
+    type: "ADD_ANIMAL_FAILED",
+    payload: err
+});
 
 export const fetchAnimals = () => {
     return (dispatch) => {
