83ff8bb6edf18c0490037a359e82530c5a5d631e
bigcatplichta <bigcatplichta@gmail.com>
2020-03-04 20:45:22 -0700

    update content

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { Container, Paper, Typography } from '@material-ui/core';
 
 const About = () => {
-    return <h1>This is the About page.</h1>
+    return (
+        <Container>
+            <Paper elevation={0}>
+                <Typography variant="h2">
+                    About WildWish
+                </Typography>
+                <Typography variant="body1">
+                    The idea that made me decide to start learning programming, WildWish is the culmination of years of experience in donating toys to zoo animals.
+                </Typography>
+                <br/>
+                <Typography variant="body1">
+                    After creating an account, zookeepers (or their zoo) can add their animals. Each animal has a list of wishes. These 
+                    wishes are featured on the landing page, where users can donate to each one. 
+                </Typography>
+                <br/>
+                <Typography variant="body2">
+                    Major TODOs:
+                <ul> 
+                    <li><Typography variant="body2">ADD STYLING TO PAGES</Typography></li>
+                    <li><Typography variant="body2">ADD FULL CRUD ACTIONS</Typography></li>
+                    <li><Typography variant="body2">ADD IMAGE UPLOAD CAPABILITIES (NEED TO CONFIGURE ACTIVESTORAGE WITH RAILS</Typography></li>
+                    <li><Typography variant="body2">CREATE LANDING PAGE</Typography></li>
+                    <li><Typography variant="body2">ACCOUNT PAGE FOR USERS</Typography></li>
+                    <li><Typography variant="body2">DONATION CAPABILITIES</Typography></li>
+                    <li><Typography variant="body2">SO MUCH MORE OMG</Typography></li>
+                </ul>
+                </Typography>
+                
+            </Paper>
+        </Container>
+    )
 }
 
 export default About
