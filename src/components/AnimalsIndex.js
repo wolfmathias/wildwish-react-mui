1d7a8c4717077d2e76673116e0156625b5e90161
bigcatplichta <bigcatplichta@gmail.com>
2020-03-04 20:45:49 -0700

    cleaned up

--- src/components/AnimalsIndex.js
+++ src/components/AnimalsIndex.js
@@ -5,27 +5,20 @@ import { Route, Switch } from 'react-router-dom';
 import NewAnimal from './NewAnimal';
 import { connect } from 'react-redux';
 
-function AnimalsIndex({match, animals}) {
-//    debugger
+// match given by routerprops. When not using redux, animals is also passed in from parent: AnimalsIndex({ match, animals })
+function AnimalsIndex(props) {
 
 // TODO: Change to ES6 class, or make parent component Animals Page that has routes for everything
-console.log({match, animals})
     return (
+        
         <>
         <Switch>
-        <Route exact path="/animals" render={routerProps => <AnimalGrid {...routerProps} animals={animals}/>} />
-        <Route path={`${match.url}/new`} render={routerProps => <NewAnimal {...routerProps} animals={animals} /> }/>
-        <Route path={`${match.url}/:animalId`} render={routerProps => <ShowAnimal {...routerProps} animals={animals.animals} /> }/>
+            <Route exact path="/animals" render={routerProps => <AnimalGrid {...routerProps} />} />
+            <Route path={`${props.match.url}/new`} render={routerProps => <NewAnimal {...routerProps} /> }/>
+            <Route path={`${props.match.url}/:animalId`} render={routerProps => <ShowAnimal {...routerProps} /> }/>
         </Switch>
-        
         </>
-
-        // Have options for views alternate than grid for main index, use list for larger screens
     )
-
-    // Can use routes to show index or individual animals
-    // <Route exact path="/" render={<AnimalGrid animals={props.animals}/>} />
-    // <Route path={`${match.url}/:animalId`} render={routerProps => <ShowAnimal {...routerProps} animals={animals} /> }/>
 }
 
 // Use redux to connect state from store to current props
