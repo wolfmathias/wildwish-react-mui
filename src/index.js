5b631506ac2f6151f836c9735082d8561b5ecbe2
bigcatplichta <bigcatplichta@gmail.com>
2020-03-10 12:08:19 -0700

    update createStore()

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 import Root from './Root';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(
-  rootReducer,
+// const store = createStore(
+//   rootReducer,
+//   applyMiddleware(thunk),
+//   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
+const store = createStore(rootReducer, composeWithDevTools(
   applyMiddleware(thunk),
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+  // other store enhancers if any
+));
 
 ReactDOM.render(<Root store={store} />, document.getElementById('root'))
