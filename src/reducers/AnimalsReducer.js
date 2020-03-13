6614d911272686d70cea7b674e3553ae614d1ffc
bigcatplichta <bigcatplichta@gmail.com>
2020-03-12 18:02:13 -0700

    update DELETE ANIMAL

--- src/reducers/AnimalsReducer.js
+++ src/reducers/AnimalsReducer.js
@@ -12,16 +12,13 @@ export default function AnimalsReducer(
             return state.concat(action.payload)
         
         case "DELETE_ANIMAL":
-            // Which of the two returns to use below? Both do same thing:
+            // Below will delete the animal but not rerender the state. Why?
             // let idx = state.findIndex(animal => animal.id  === action.payload)
             // return [...state.slice(0, idx), ...state.slice(idx + 1)];
-            return {
-                ...state,
-                animals: state.animals.filter(animal => animal.id !== action.payload.id),
-            }
+
+            return state.filter(animal => animal.id !== action.payload.id)
 
         // Both AnimalReducer and WishesReducer respond to ADD WISH action
-        // TODO: update prop names to match actual data
         // Do I even want to handle it this way?
         // case "ADD_WISH":
         //     let existingAnimal = state.filter(
