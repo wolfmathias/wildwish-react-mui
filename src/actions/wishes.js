0fbc5fbe870c69d99d31a1d90c4220ebb139ce04
bigcatplichta <bigcatplichta@gmail.com>
2020-03-16 18:54:34 -0600

    comment action

--- src/actions/wishes.js
+++ src/actions/wishes.js
@@ -5,24 +5,25 @@ export const fetchWishes = () => {
     return (dispatch) => {
         // dispatch action to change state.isFetching to 'true'
         // TODO: create LoadingReducer and modify structure of state/store
-        dispatch({type: 'LOADING_RESOURCES'})
+        // dispatch({type: 'WISH_LOAD_START'})
         fetch(BASE_URL.concat(ACTIVE_WISHES))
         .then(resp => resp.json())
         .then(respJSON => {
             dispatch({ type: 'ADD_WISHES', payload: respJSON })
+            // dispatch({ type: 'WISH_LOAD_END })
         })
     }
 }
 
 // Rewrite fetchWIshes as general method:
-export const fetchResource = (route, respAction) => {
-    return (dispatch) => {
-      dispatch({ type: 'LOADING_RESOURCES'})
-      fetch(BASE_URL.concat(route))
-      .then(response => response.json())
-      .then(responseJSON => {
-        console.log(responseJSON)
-        dispatch({ type: respAction, payload: responseJSON })
-      })
-    }
-}
+// export const fetchResource = (route, respAction) => {
+//     return (dispatch) => {
+//       dispatch({ type: 'LOADING_RESOURCES'})
+//       fetch(BASE_URL.concat(route))
+//       .then(response => response.json())
+//       .then(responseJSON => {
+//         console.log(responseJSON)
+//         dispatch({ type: respAction, payload: responseJSON })
+//       })
+//     }
+// }
