b90d40e706686a4b0bb0e695428929f7a82d5b7e
bigcatplichta <bigcatplichta@gmail.com>
2020-03-16 18:43:12 -0600

    remove uuid

--- src/reducers/AnimalsReducer.js
+++ src/reducers/AnimalsReducer.js
@@ -1,5 +1,3 @@
-import uuid from "uuid";
-
 export default function AnimalsReducer(
     state = [], 
     action
@@ -12,13 +10,14 @@ export default function AnimalsReducer(
             return state.concat(action.payload)
         
         case "DELETE_ANIMAL":
-            // Below will delete the animal but not rerender the state. Why?
+            // Below will delete the animal but not rerender the state. Why
             // let idx = state.findIndex(animal => animal.id  === action.payload)
             // return [...state.slice(0, idx), ...state.slice(idx + 1)];
 
             return state.filter(animal => animal.id !== action.payload.id)
 
         // Both AnimalReducer and WishesReducer respond to ADD WISH action
+        // TODO: update prop names to match actual data
         // Do I even want to handle it this way?
         // case "ADD_WISH":
         //     let existingAnimal = state.filter(
