527cd70e7de771b6eedac2f601272014c1c4f9a3
bigcatplichta <bigcatplichta@gmail.com>
2020-06-24 21:02:12 -0600

    create file and loginUser action

---
+++ src/actions/userActions.js
@@ -0,0 +1,24 @@
+// copied from add animal action
+
+export const loginUser = (username, password) => {
+    const LOGIN_URL = 'http://localhost:3001/users/sign_in'
+    const LOGIN_OPTIONS = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+    }
+    
+    return (dispatch) => {
+        dispatch({ type: 'USER_LOGIN_START' })
+        fetch(LOGIN_URL, LOGIN_OPTIONS)
+        .then(response => response.json())
+        .then(responseJSON => {
+            // console.log("Posted to the server!")
+            dispatch({ type: 'LOGIN_USER', payload: responseJSON })
+            dispatch( { type: 'USER_LOGIN_END' })
+        })
+    }
+}
