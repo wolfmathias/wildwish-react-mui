5863686b377f7fe8773f5a6c6e030839457912cf
bigcatplichta <bigcatplichta@gmail.com>
2020-06-26 19:54:51 -0600

    update params

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,11 +1,12 @@
 // copied from add animal action
 
 
-export const loginUser = (username, password) => {
+export const loginUser = (email, password) => {
 // TODOs:
 // store USER object in Redux store
 // set USER in browser's localStorage (same as session?)
-    
+// learn AXIOS to handle fetch requests
+
     const LOGIN_URL = 'http://localhost:3001/users/sign_in'
     const LOGIN_OPTIONS = {
         method: 'POST',
@@ -13,17 +14,18 @@ export const loginUser = (username, password) => {
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         },
-        body: JSON.stringify({ username, password })
+        body: JSON.stringify({ email, password })
     }
     
     return (dispatch) => {
+        console.log("should hit fetch request")
         dispatch({ type: 'USER_LOGIN_START' })
         fetch(LOGIN_URL, LOGIN_OPTIONS)
         .then(response => response.json())
         .then(responseJSON => {
             // console.log("Posted to the server!")
             dispatch({ type: 'LOGIN_USER', payload: responseJSON })
-            dispatch( { type: 'USER_LOGIN_END' })
+            dispatch({ type: 'USER_LOGIN_END' })
         })
     }
 }
