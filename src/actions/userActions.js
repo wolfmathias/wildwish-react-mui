53b5d0432f0570ee80b5299545b803b79467537e
bigcatplichta <bigcatplichta@gmail.com>
2020-06-28 16:30:47 -0600

    remove unused lines

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,12 +1,11 @@
 // copied from add animal action
 
-
-export const loginUser = (email, password) => {
 // TODOs:
 // store USER object in Redux store
 // set USER in browser's localStorage (same as session?)
 // learn AXIOS to handle fetch requests
 
+export const loginUser = (email, password) => {
     const LOGIN_URL = 'http://localhost:3001/users/sign_in'
     const LOGIN_OPTIONS = {
         method: 'POST',
@@ -16,16 +15,12 @@ export const loginUser = (email, password) => {
         },
         body: JSON.stringify({ email, password })
     }
-    
+
     return (dispatch) => {
-        console.log("should hit fetch request")
-        dispatch({ type: 'USER_LOGIN_START' })
         fetch(LOGIN_URL, LOGIN_OPTIONS)
         .then(response => response.json())
         .then(responseJSON => {
-            // console.log("Posted to the server!")
             dispatch({ type: 'LOGIN_USER', payload: responseJSON })
-            dispatch({ type: 'USER_LOGIN_END' })
         })
     }
 }
