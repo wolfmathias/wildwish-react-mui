0e5806b8229a922af74089271c520c865c3eab5e
bigcatplichta <bigcatplichta@gmail.com>
2020-07-14 23:15:21 -0600

    refactor to ES6 Class (why?)

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -29,96 +29,93 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
-function CreateUser(props) {
-    const classes = useStyles();
+class UserForm extends Component {
+    // classes = useStyles();
 
     // React hooks for state
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
+    // const [email, setEmail] = useState();
+    // const [password, setPassword] = useState();
 
-    const handleEmailChange = (event) => {
-        setEmail(
+    handleInputChange = (event) => {
+        setState(
             event.target.value
         );
     }
-    const handlePasswordChange = (event) => {
-        setPassword(
-            event.target.value
-        );
-    }
+    
             
     // For Oauth buttons
-    const responseGoogle = (response) => {
+    responseGoogle = (response) => {
         console.log(response);
     }
-    const responseFacebook = (response) => {
+    responseFacebook = (response) => {
         console.log(response);
     }
 
-    const handleSubmit = (event) => {
+    handleSubmit = (event, action) => {
         event.preventDefault()
         
         if (email && password) {
-            props.loginUser(email, password);
+            props.loginUser(email, password, action);
         }
     }
 
-    return (
-        <Container>
-            <Paper elevation={1} >
-                <div className={classes.login}>
-                <GoogleLogin 
-                    clientId={GOOGLE_OAUTH_ID}
-                    buttonText="Continue with Google"
-                    onSuccess={responseGoogle}
-                    onFailure={responseGoogle}
-                    cookiePolicy={'single_host_origin'}
-                />
-                <br />
-                <FacebookLogin
-                    appId={FACEBOOK_APP_ID}
-                    textButton="Continue with Facebook"
-                    size="medium"
-                    fields="name,email"
-                    callback={responseFacebook} 
-                />
-                <br />
-                <Typography>Or continue with email:</Typography>
-                <form name="login" onSubmit={handleSubmit}>
-                    <TextField 
-                        fullWidth 
-                        id="email"
-                        name="email"
-                        label="Email"
-                        variant="outlined"
-                        required onChange={handleEmailChange}
+    render() {
+        return (
+            <Container>
+                <Paper elevation={1} >
+                    <div className={classes.login}>
+                    <GoogleLogin 
+                        clientId={GOOGLE_OAUTH_ID}
+                        buttonText="Continue with Google"
+                        onSuccess={responseGoogle}
+                        onFailure={responseGoogle}
+                        cookiePolicy={'single_host_origin'}
                     />
-                    <TextField
-                        fullWidth
-                        id="password"
-                        name="password"
-                        type="password"
-                        label="Password"
-                        variant="outlined"
-                        required
-                        onChange={handlePasswordChange}
-                        style={{ marginTop: 10 }}
+                    <br />
+                    <FacebookLogin
+                        appId={FACEBOOK_APP_ID}
+                        textButton="Continue with Facebook"
+                        size="medium"
+                        fields="name,email"
+                        callback={responseFacebook} 
                     />
-                    <Button type="submit" color="primary" variant="outlined">
-                        Submit
-                    </Button>
-                </form>
-                <br />
-                <Grid container justify="flex-end">
-                    <Grid item>
-                        <Link href="#" variant="body2">
-                            Create an account
-                        </Link>
+                    <br />
+                    <Typography>Or continue with email:</Typography>
+                    <form name="login" onSubmit={handleSubmit}>
+                        <TextField 
+                            fullWidth 
+                            id="email"
+                            name="email"
+                            label="Email"
+                            variant="outlined"
+                            required onChange={handleEmailChange}
+                        />
+                        <TextField
+                            fullWidth
+                            id="password"
+                            name="password"
+                            type="password"
+                            label="Password"
+                            variant="outlined"
+                            required
+                            onChange={handlePasswordChange}
+                            style={{ marginTop: 10 }}
+                        />
+                        <Button type="submit" color="primary" variant="outlined">
+                            Submit
+                        </Button>
+                    </form>
+                    <br />
+                    <Grid container justify="flex-end">
+                        <Grid item>
+                            <Link href="#" variant="body2">
+                                Create an account
+                            </Link>
+                        </Grid>
                     </Grid>
-                </Grid>
-                </div>
-            </Paper>
-        </Container>
-    )
+                    </div>
+                </Paper>
+            </Container>
+        )
+    }
 }
-export default connect(null, { createUser })(CreateUser);
