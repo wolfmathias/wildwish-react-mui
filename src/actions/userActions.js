1db0d94d2acfc0aad9134c6e7ba7b10e86d858f7
bigcatplichta <bigcatplichta@gmail.com>
2020-07-14 22:50:07 -0600

    add createUser action

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -4,9 +4,30 @@
 // store USER object in Redux store
 // set USER in browser's localStorage (same as session?)
 // learn AXIOS to handle fetch requests
+// refactor to reduce boilerplate
 
 export const loginUser = (email, password) => {
     const LOGIN_URL = 'http://localhost:3001/users/sign_in'
+    const LOGIN_OPTIONS = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    }
+
+    return (dispatch) => {
+        fetch(LOGIN_URL, LOGIN_OPTIONS)
+        .then(response => response.json())
+        .then(responseJSON => {
+            dispatch({ type: 'LOGIN_USER', payload: responseJSON })
+        })
+    }
+}
+
+export const createUser = (email, password) => {
+    const LOGIN_URL = 'http://localhost:3001/users/sign_up'
     const LOGIN_OPTIONS = {
         method: 'POST',
         headers: {
